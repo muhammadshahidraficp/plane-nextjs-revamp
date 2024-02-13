ca7b39ff4cbb37bd81b79512c736437840b88fa2
shahidrafi <muhammadshahidraficp@gmail.com>
2024-02-13 17:37:35 +0400

    initial commmit

---
+++ components/project/card-list.tsx
@@ -0,0 +1,81 @@
+import { observer } from "mobx-react-lite";
+import { useTheme } from "next-themes";
+// hooks
+import { useApplication, useEventTracker, useProject, useUser } from "hooks/store";
+// components
+import { ProjectCard } from "components/project";
+import { Loader } from "@plane/ui";
+import { EmptyState, getEmptyStateImagePath } from "components/empty-state";
+// constants
+import { EUserWorkspaceRoles } from "constants/workspace";
+
+export const ProjectCardList = observer(() => {
+  // theme
+  const { resolvedTheme } = useTheme();
+  // store hooks
+  const { commandPalette: commandPaletteStore } = useApplication();
+  const { setTrackElement } = useEventTracker();
+  const {
+    membership: { currentWorkspaceRole },
+    currentUser,
+  } = useUser();
+  const { workspaceProjectIds, searchedProjects, getProjectById } = useProject();
+
+  const isLightMode = resolvedTheme ? resolvedTheme === "light" : currentUser?.theme.theme === "light";
+  const emptyStateImage = getEmptyStateImagePath("onboarding", "projects", isLightMode);
+
+  const isEditingAllowed = !!currentWorkspaceRole && currentWorkspaceRole >= EUserWorkspaceRoles.MEMBER;
+
+  if (!workspaceProjectIds)
+    return (
+      <Loader className="grid grid-cols-3 gap-4">
+        <Loader.Item height="100px" />
+        <Loader.Item height="100px" />
+        <Loader.Item height="100px" />
+        <Loader.Item height="100px" />
+        <Loader.Item height="100px" />
+        <Loader.Item height="100px" />
+      </Loader>
+    );
+
+  return (
+    <>
+      {workspaceProjectIds.length > 0 ? (
+        <div className="h-full w-full overflow-y-auto p-8">
+          {searchedProjects.length == 0 ? (
+            <div className="mt-10 w-full text-center text-custom-text-400">No matching projects</div>
+          ) : (
+            <div className="grid grid-cols-1 gap-9 md:grid-cols-2 lg:grid-cols-3">
+              {searchedProjects.map((projectId) => {
+                const projectDetails = getProjectById(projectId);
+
+                if (!projectDetails) return;
+
+                return <ProjectCard key={projectDetails.id} project={projectDetails} />;
+              })}
+            </div>
+          )}
+        </div>
+      ) : (
+        <EmptyState
+          image={emptyStateImage}
+          title="Start a Project"
+          description="Think of each project as the parent for goal-oriented work. Projects are where Jobs, Cycles, and Modules live and, along with your colleagues, help you achieve that goal."
+          primaryButton={{
+            text: "Start your first project",
+            onClick: () => {
+              setTrackElement("Project empty state");
+              commandPaletteStore.toggleCreateProjectModal(true);
+            },
+          }}
+          comicBox={{
+            title: "Everything starts with a project in Plane",
+            description: "A project could be a product’s roadmap, a marketing campaign, or launching a new car.",
+          }}
+          size="lg"
+          disabled={!isEditingAllowed}
+        />
+      )}
+    </>
+  );
+});
---
+++ layouts/app-layout/sidebar.tsx
@@ -0,0 +1,66 @@
+'use client'
+import { FC, useEffect, useRef } from "react";
+import { observer } from "mobx-react-lite";
+// components
+import {
+  WorkspaceHelpSection,
+  WorkspaceSidebarDropdown,
+  WorkspaceSidebarMenu,
+  WorkspaceSidebarQuickAction,
+} from "../../components/workspace";
+import { ProjectSidebarList } from "../../components/project";
+// hooks
+import { useApplication } from "../../hooks/store";
+import useOutsideClickDetector from "../../hooks/use-outside-click-detector";
+
+export interface IAppSidebar { }
+
+export const AppSidebar: FC<IAppSidebar> = observer(() => {
+  // store hooks
+  const { theme: themStore } = useApplication();
+  const ref = useRef<HTMLDivElement>(null);
+
+  useOutsideClickDetector(ref, () => {
+    if (themStore.sidebarCollapsed === false) {
+      if (window.innerWidth < 768) {
+        themStore.toggleSidebar();
+      }
+    }
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        themStore.toggleSidebar(true);
+      }
+      if (window.innerWidth > 768) {
+        themStore.toggleSidebar(false);
+      }
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [themStore]);
+
+  return (
+    <div
+      className={`inset-y-0 z-20 flex h-full flex-shrink-0 flex-grow-0 flex-col border-r border-custom-sidebar-border-200 bg-custom-sidebar-background-100 duration-300
+        fixed md:relative
+        ${themStore.sidebarCollapsed ? "-ml-[280px]" : ""}
+        sm:${themStore.sidebarCollapsed ? "-ml-[280px]" : ""}
+        md:ml-0 ${themStore.sidebarCollapsed ? "w-[80px]" : "w-[280px]"}
+        lg:ml-0 ${themStore.sidebarCollapsed ? "w-[80px]" : "w-[280px]"}
+      `}
+    >
+      <div ref={ref} className="flex h-full w-full flex-1 flex-col">
+        <WorkspaceSidebarDropdown />
+        <WorkspaceSidebarQuickAction />
+        <WorkspaceSidebarMenu />
+        <ProjectSidebarList />
+        <WorkspaceHelpSection />
+      </div>
+    </div>
+  );
+});
